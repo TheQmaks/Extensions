f36195c094a6257fd94a84a36b8c553444b6f647
Anatoliy <11361354+TheQmaks@users.noreply.github.com>
2021-06-27 21:22:09 +0300

    Update script.user.js

--- script.user.js
+++ script.user.js
@@ -9,32 +9,30 @@
 // ==/UserScript==
 
 (function() {
-   $(document).ajaxSuccess(
-       function(event, xhr, settings){
-           if(settings.url.includes("wbxcatalog-ru.wildberries.ru")) {
-               document.querySelectorAll(".block-dop-info:not(.il)").forEach(item => {
-                   let id = item.parentNode.parentNode.getAttribute("data-popup-nm-id");
-                   let part = id.slice(0, -6);
+    function process() {
+        document.querySelectorAll(".block-dop-info:not(.processed)").forEach(item => {
+            let id = item.parentNode.parentNode.getAttribute("data-popup-nm-id");
+            let part = id.slice(0, -6);
 
-                   let request = new XMLHttpRequest();
-                   request.open('GET', `https://wbxcatalog-eu.wildberries.ru/part/${part}/catalog?locale=il&product=` + id);
-                   request.onload = function() {
-                       if (!item.classList.contains("il") && request.status == 200 && !request.response.includes("products\":[]") && !request.response.includes("disabledForRegion\":true")) {
-                           let il = document.createElement("a");
+            let request = new XMLHttpRequest();
+            request.open('GET', `https://wbxcatalog-eu.wildberries.ru/part/${part}/catalog?locale=il&product=` + id);
+            request.onload = function() {
+                if (!item.classList.contains("processed") && request.status == 200 && !request.response.includes("products\":[]") && !request.response.includes("disabledForRegion\":true")) {
+                    let il = document.createElement("a");
 
-                           il.target = "_blank";
-                           il.href = "https://wildberries.co.il/product?card=" + id;
-                           il.className = "spec-actions-catalog i-spec-action";
-                           il.style.background = "linear-gradient(90deg,#9da3e6 0,#eaeaea 95.32%)";
-                           il.innerHTML = "ISRAEL";
+                    il.target = "_blank";
+                    il.href = "https://wildberries.co.il/product?card=" + id;
+                    il.className = "spec-actions-catalog i-spec-action";
+                    il.style.background = "linear-gradient(90deg,#9da3e6 0,#eaeaea 95.32%)";
+                    il.innerHTML = "ISRAEL";
 
-                           item.classList.add("il");
-                           item.appendChild(il);
-                       }
-                   }
-                   request.send();
-               });
-           }
-       }
-   );
+                    item.appendChild(il);
+                }
+                item.classList.add("processed");
+            }
+            request.send();
+        });
+    }
+
+    setInterval(process, 5000);
 })();
