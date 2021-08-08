881be44572c9a7dc113dddd57a66c57cde1b4dd7
Anatoliy <11361354+TheQmaks@users.noreply.github.com>
2021-08-09 00:08:37 +0300

    Update script.user.js

--- script.user.js
+++ script.user.js
@@ -1,39 +1,7 @@
 // ==UserScript==
 // @name         Wildberries IL
-// @version 0.2
-// @updateURL https://github.com/TheQmaks/Wildberries-IL/raw/main/script.user.js
-// @namespace    https://github.com/TheQmaks
 // @description  Wildberries RU to Israel product availability checker
-// @author       AnatoliyFeed
+// @require https://github.com/TheQmaks/Wildberries-IL/raw/main/extension.js
 // @match        https://www.wildberries.ru/*
 // @icon         https://www.wildberries.ru/favicon.ico
 // ==/UserScript==
-
-(function() {
-    function process() {
-        document.querySelectorAll(".block-dop-info:not(.processed)").forEach(item => {
-            let id = item.parentNode.parentNode.getAttribute("data-nm-id");
-            let part = id.slice(0, -6);
-
-            let request = new XMLHttpRequest();
-            request.open('GET', `https://wbxcatalog-eu.wildberries.ru/part/${part}/catalog?locale=il&product=${id}&regions=1,4,30,38,40,48,65,68,69,78&stores=507,117501`);
-            request.onload = function() {
-                if (!item.classList.contains("processed") && request.status == 200 && request.response.includes("stocks\":[{")) {
-                    let il = document.createElement("a");
-
-                    il.target = "_blank";
-                    il.href = "https://wildberries.co.il/product?card=" + id;
-                    il.className = "spec-actions-catalog i-spec-action";
-                    il.style.background = "linear-gradient(90deg,#9da3e6 0,#eaeaea 95.32%)";
-                    il.innerHTML = "ISRAEL";
-
-                    item.appendChild(il);
-                }
-                item.classList.add("processed");
-            }
-            request.send();
-        });
-    }
-
-    setInterval(process, 5000);
-})();
