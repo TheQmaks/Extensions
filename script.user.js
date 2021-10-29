c739b4984129ab5ca2a1b21c1a65f5aad6814a29
Anatoliy <11361354+TheQmaks@users.noreply.github.com>
2021-10-29 23:40:33 +0300

    Update script.user.js

--- script.user.js
+++ script.user.js
@@ -2,7 +2,6 @@
 // @name         Wildberries IL
 // @description  Wildberries RU to Israel product availability checker
 // @require https://github.com/TheQmaks/Wildberries-IL/raw/main/extension.js
-// @include      https://wildberries.ru/*
-// @include      https://wildberries.co.il/*
+// @include      https://www.wildberries.ru/*
 // @icon         https://wildberries.co.il/assets/app-icon-180x180.png
 // ==/UserScript==
