03a215c06b4925dfbe1c908657454baa0ed9fbbf
Anatoliy <11361354+TheQmaks@users.noreply.github.com>
2022-04-21 20:36:01 +0300

    Add files via upload

---
+++ lcwaikiki/script.user.js
@@ -0,0 +1,30 @@
+// ==UserScript==
+// @name         LCWaikiki CUCO
+// @description  Automatic currency converter for LCWaikiki from turkish lira (TL) to new israeli shekel (ILS)
+// @match        https://www.lcwaikiki.com/*
+// @icon         https://www.lcwaikiki.com/Resource/Images/favicon.ico
+// @connect      bankhapoalim.co.il
+// @grant        GM_xmlhttpRequest
+// ==/UserScript==
+
+var exchangeRate = 0;
+
+GM_xmlhttpRequest({
+    method: "GET",
+    url: "https://www.bankhapoalim.co.il/he/_foreign/chart/387",
+    onload: function(response) {
+        let json = JSON.parse(response.responseText);
+        exchangeRate = json[0]["week"][json[1]];
+    }
+});
+
+function process() {
+    document.querySelectorAll("span[class*='price']").forEach(item => {
+        let text = item.innerText;
+        if(text.endsWith("TL")) {
+            item.innerText = (Number((parseFloat(text.split(" ")[0]) * exchangeRate)).toFixed(1)) + "₪";
+        }
+    });
+}
+
+setInterval(process, 5000);
