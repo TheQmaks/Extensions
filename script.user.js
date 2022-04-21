80d76295f41c569654cd76eb7e06f82e7d03bea5
Anatoliy <11361354+TheQmaks@users.noreply.github.com>
2022-04-21 20:42:02 +0300

    Delete script.user.js

--- script.user.js
+++
@@ -1,7 +0,0 @@
-// ==UserScript==
-// @name         Wildberries IL
-// @description  Wildberries RU to Israel product availability checker
-// @require https://github.com/TheQmaks/Wildberries-IL/raw/main/extension.js
-// @include      https://www.wildberries.ru/*
-// @icon         https://wildberries.co.il/assets/app-icon-180x180.png
-// ==/UserScript==
