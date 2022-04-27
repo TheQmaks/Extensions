76327fbcbd05b45e4f389c4428810b7995649751
Anatoliy <11361354+TheQmaks@users.noreply.github.com>
2022-04-27 14:51:22 +0300

    Update script.user.js

--- lcwaikiki/script.user.js
+++ lcwaikiki/script.user.js
@@ -1,29 +1,39 @@
 // ==UserScript==
 // @name         LCWaikiki CUCO
 // @description  Automatic currency converter for LCWaikiki from turkish lira (TL) to new israeli shekel (ILS)
-// @version      0.1
+// @version      0.2
 // @match        https://www.lcwaikiki.com/*
 // @icon         https://www.lcwaikiki.com/Resource/Images/favicon.ico
 // @connect      bankhapoalim.co.il
 // @grant        GM_xmlhttpRequest
 // ==/UserScript==
 
-var exchangeRate = 0;
+var exchangeRateTL = 0;
+var exchangeRateUSD = 0;
 
 GM_xmlhttpRequest({
     method: "GET",
     url: "https://www.bankhapoalim.co.il/he/_foreign/chart/387",
     onload: function(response) {
         let json = JSON.parse(response.responseText);
-        exchangeRate = json[0]["week"][json[1]];
+        exchangeRateTL = json[0]["week"][json[1]];
+    }
+});
+GM_xmlhttpRequest({
+    method: "GET",
+    url: "https://www.bankhapoalim.co.il/he/_foreign/chart/19",
+    onload: function(response) {
+        let json = JSON.parse(response.responseText);
+        exchangeRateUSD = json[0]["week"][json[1]];
     }
 });
 
 function process() {
-    document.querySelectorAll("span[class*='price']").forEach(item => {
-        let text = item.innerText;
+    document.querySelectorAll("span[class*='price-regular'], div[class*='basket-discount']").forEach(element => {
+        let text = element.innerText;
         if(text.endsWith("TL")) {
-            item.innerText = (Number((parseFloat(text.split(" ")[0]) * exchangeRate)).toFixed(1)) + "₪";
+            let priceILS = parseFloat(text.split(" ")[0]) * exchangeRateTL;
+            element.innerText = Number(priceILS / exchangeRateUSD).toFixed(1) + "$ (" + Number(priceILS).toFixed(1) + "₪)";
         }
     });
 }
